3ba8c323108ba69cfe248204ce51ea93e0790bfb
cheeryguo <cheery.guo@gmail.com>
2016-04-28 07:33:09 -0500

    Created by Coding Ground Client

---
+++ New Project-20160428/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import cmath
+if True :
+	print('AABBcc天天向上')
+	aaa=11
+	print(aaa)
+	print(sys.version)
+print cmath.sqrt(-1)
+import math
+print math.sqrt(16)
+
